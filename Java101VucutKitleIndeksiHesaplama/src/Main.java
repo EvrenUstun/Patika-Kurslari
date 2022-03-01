0cf87ed2d2ec8e3bd7a293ae818494a89629700e
EvrenUstun <ustunnevren@gmail.com>
2022-03-01 10:15:46 +0300

    Vücut Kitle İndeksi Hesaplama

---
+++ Java101VucutKitleIndeksiHesaplama/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double kg, boy, indeks;
+
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+        boy = input.nextDouble();
+
+        System.out.println("Lütfen kilonuzu giriniz: ");
+        kg = input.nextDouble();
+
+        indeks = kg / (boy * boy);
+
+        System.out.println("Vücut Kitle İndeksiniz: " + indeks);
+    }
+}
