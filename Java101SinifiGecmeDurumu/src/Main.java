f523fdd8d9f8e9908499274ee4666026aa9145fc
EvrenUstun <ustunnevren@gmail.com>
2022-03-01 13:14:00 +0300

    Sınıfı Geçme Durumu

---
+++ Java101SinifiGecmeDurumu/src/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int mat, fzk, kmy, trc, mzk;
+        double dersSayisi = 0;
+        double toplam = 0;
+        double ortalama = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Matematik Notunuz: ");
+        mat = input.nextInt();
+        if (mat >= 0 & mat <= 100) {
+            dersSayisi += 1;
+            toplam += mat;
+        }
+
+        System.out.print("Fizik Notunuz: ");
+        fzk = input.nextInt();
+        if (fzk >= 0 & fzk <= 100) {
+            dersSayisi += 1;
+            toplam += fzk;
+        }
+
+        System.out.print("Kimya Notunuz: ");
+        kmy = input.nextInt();
+        if (kmy >= 0 & kmy <= 100) {
+            dersSayisi += 1;
+            toplam += kmy;
+        }
+
+        System.out.print("Türkçe Notunuz: ");
+        trc = input.nextInt();
+        if (trc >= 0 & trc <= 100) {
+            dersSayisi += 1;
+            toplam += trc;
+        }
+
+        System.out.print("Müzik Notunuz: ");
+        mzk = input.nextInt();
+        if (mzk >= 0 & mzk <= 100) {
+            dersSayisi += 1;
+            toplam += mzk;
+        }
+
+        ortalama = toplam / dersSayisi;
+
+        System.out.println("Ortalamanız: " + ortalama);
+
+        String gecmeDurumu = (ortalama > 55) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(gecmeDurumu);
+    }
+}
