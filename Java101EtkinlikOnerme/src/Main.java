0edbe66ee1e19c419c20a9749699ec6ef7e4082f
EvrenUstun <ustunnevren@gmail.com>
2022-03-01 13:16:40 +0300

    Etkinlik Önerme

---
+++ Java101EtkinlikOnerme/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random rnd = new Random();
+
+        int low = -5;
+        int high = 35;
+        int temp = rnd.nextInt(high - low) + low;
+        String etkinlik;
+
+        if (temp < 5) {
+
+            etkinlik = "Kayak yapmaya gidebilirsiniz.";
+        } else if (temp >= 5 & temp < 15) {
+            etkinlik = "Sinemaya gidebilirsiniz.";
+        } else if (temp >= 15 & temp < 25) {
+            etkinlik = "Pikniğe gidebilirsiniz.";
+        } else {
+            etkinlik = "Yüzmeye gidebilirsiniz.";
+        }
+
+        System.out.print("Hava sıcaklığı " + temp + " derece. " + etkinlik);
+
+    }
+}
