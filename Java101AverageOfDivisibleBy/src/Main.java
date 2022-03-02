441937c6b784f936d04f6dfecf798dcc5aa41def
EvrenUstun <ustunnevren@gmail.com>
2022-03-02 10:58:34 +0300

    Tam Bölünme

---
+++ Java101AverageOfDivisibleBy/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int c;
+        int divisibleTotal = 0;
+        int divisibleCount = 0;
+        int divisibleAverage;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayı giriniz : ");
+        c = input.nextInt();
+
+        for (int i = 0; i <= c; i++) {
+            if (i > 0) {
+                if (i % 3 == 0 && i % 4 == 0) {
+                    divisibleTotal += i;
+                    divisibleCount += 1;
+                }
+            }
+        }
+
+        divisibleAverage = (divisibleTotal / divisibleCount);
+        System.out.print(c + " Dahil olmak üzere bu değere kadar, üçe ve dörde tam olarak bölünebilen sayıların ortalaması : ");
+        System.out.println(divisibleAverage);
+
+    }
+}
